86b27ba3a361f367d0fa6485b1a286d68bdfba4b
Vincent Rémond <vincent.remond@gmail.com>
2021-05-28 11:22:24 +0200

    Clone project

---
+++ CloneProject/Console.fs
@@ -0,0 +1,15 @@
+﻿module CloneProject
+
+open System.Runtime.InteropServices
+
+module ConsoleConfiguration =
+    let StdOutputHandle: uint = 0xFFFFFFF5u
+
+    [<DllImport("kernel32.dll", SetLastError = true)>]
+    extern bool AllocConsole()
+
+    [<DllImport("kernel32.dll")>]
+    extern nativeint GetStdHandle(uint nStdHandle)
+
+    [<DllImport("kernel32.dll")>]
+    extern void SetStdHandle(uint nStdHandle, nativeint handle)
---
+++ CloneProject/Program.fs
@@ -0,0 +1,69 @@
+open System
+open System.Diagnostics
+open System.IO
+open System.Runtime.CompilerServices
+open System.Text
+open System.Text.RegularExpressions
+open System.Threading
+open System.Windows.Forms
+open CloneProject
+open RunProcess
+
+let write name contents = File.WriteAllText(name, contents)
+
+let explorer (path: string) =
+    Process.Start("explorer", path).WaitForExit()
+
+let clone gitUrl targetDirectory =
+    use proc =
+        new ProcessHost("git.exe", targetDirectory)
+
+    proc.Start($"clone {gitUrl}")
+    proc.WaitForExit(TimeSpan.MaxValue) |> ignore
+    let stdErr = proc.StdErr.ReadAllText(Encoding.UTF8)
+    let stdOut = proc.StdOut.ReadAllText(Encoding.UTF8)
+    printfn "%s" stdErr
+    printfn "%s" stdOut
+    stdErr
+
+let readOutputDirectory output =
+    Regex
+        .Match(output, "\'(?<OutputDirectory>.+?)\'")
+        .Groups.["OutputDirectory"]
+        .Value
+
+let showConsole =
+    ConsoleConfiguration.AllocConsole() |> ignore
+
+    let defaultStdout = nativeint 7
+
+    match ConsoleConfiguration.GetStdHandle(ConsoleConfiguration.StdOutputHandle) = defaultStdout with
+    | false -> ConsoleConfiguration.SetStdHandle(ConsoleConfiguration.StdOutputHandle, defaultStdout)
+    | _ -> ()
+
+[<EntryPoint>]
+[<STAThread>]
+let main argv =
+    showConsole
+    printfn "%b" (ConsoleConfiguration.AllocConsole())
+
+    let targetDirectory =
+        Seq.tryHead argv |> Option.defaultValue @"D:\GIT\"
+
+    let gitUrl = Clipboard.GetText()
+
+    printfn "Cloning %s into %s" gitUrl targetDirectory
+
+    let outputDirectory =
+        (clone gitUrl targetDirectory)
+        |> readOutputDirectory
+
+    let path =
+        Path.Combine(targetDirectory, outputDirectory)
+
+    explorer path
+
+    printfn "Project cloned into %s" path
+    Thread.Sleep(TimeSpan.FromSeconds(10.))
+
+    0

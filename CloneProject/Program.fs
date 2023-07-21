5c7222fab4b1eb573e3439c8d2c2902c9a3ffd72
Vincent Rémond <vincent.remond@gmail.com>
2023-07-21 11:51:50 +0200

    2023-07-21

--- CloneProject/Program.fs
+++ CloneProject/Program.fs
@@ -18,16 +18,20 @@ module Process =
             .Start(ProcessStartInfo(name, arguments, WorkingDirectory = directory))
             .WaitForExit()
 
-
 let explorer (path: string) = Process.start "explorer.exe" path path
 
 let fork (path: string) = Process.start "fork.exe" path path
+
+let rider (path: string) = Process.start "powershell.exe" path "-Command rider.ps1"
 
 let clone gitUrl targetDirectory =
     use proc = new ProcessHost("git.exe", targetDirectory)
 
     proc.Start($"clone {gitUrl}")
-    proc.WaitForExit(TimeSpan.MaxValue) |> ignore
+
+    proc.WaitForExit(TimeSpan.MaxValue)
+    |> ignore
+
     let stdErr = proc.StdErr.ReadAllText(Encoding.UTF8)
     let stdOut = proc.StdOut.ReadAllText(Encoding.UTF8)
     printfn $"%s{stdErr}"
@@ -41,7 +45,8 @@ let readOutputDirectory output =
         .Value
 
 let showConsole =
-    ConsoleConfiguration.AllocConsole() |> ignore
+    ConsoleConfiguration.AllocConsole()
+    |> ignore
 
     let defaultStdout = nativeint 7
 
@@ -68,8 +73,21 @@ let (<!>) (a: 'a option) (b: unit -> 'a option) =
     | None -> b ()
 
 [<RequireQualifiedAccess>]
+module List =
+    let tryRemove (value: 'a) (list: 'a list) : 'a list * bool =
+        List.foldBack
+            (fun item (acc, found) -> if item = value then (acc, true) else (item :: acc, found))
+            list
+            ([], false)
+
+[<RequireQualifiedAccess>]
 module Uri =
     let getHost (uri: Uri) = uri.Host
+
+[<RequireQualifiedAccess>]
+module Regex =
+    let replace (pattern: string) (replacement: string) input =
+        Regex.Replace(input, pattern, replacement)
 
 [<RequireQualifiedAccess>]
 module Map =
@@ -84,9 +102,12 @@ let deductTargetDirectory (cliParam: string option) (url: string) : string optio
         |> Option.bind (config.Value |> Map.tryFind')
 
     let defaultValue () =
-        "Default" |> (config.Value |> Map.tryFind')
+        "Default"
+        |> (config.Value |> Map.tryFind')
 
-    cliParam <!> (tryFindByDomain url) <!> defaultValue
+    cliParam
+    <!> (tryFindByDomain url)
+    <!> defaultValue
 
 [<EntryPoint>]
 [<STAThread>]
@@ -94,25 +115,44 @@ let main argv =
     showConsole
     printfn $"%b{ConsoleConfiguration.AllocConsole()}"
 
+    let argv, mergeRequest =
+        argv
+        |> Seq.toList
+        |> List.tryRemove "--merge-request"
+
     let gitUrl = Clipboard.GetText()
 
+    let gitUrl =
+        if mergeRequest then
+            gitUrl
+            |> Regex.replace "/-/merge_requests/.*$" ".git"
+        else
+            gitUrl
+
     let targetDirectory =
-        deductTargetDirectory (argv |> Array.tryHead) gitUrl
+        deductTargetDirectory (argv |> List.tryHead) gitUrl
         |> Option.defaultWith (fun () -> failwith "No target directory found")
-
-
 
     printfn $"Cloning %s{gitUrl} into %s{targetDirectory}"
 
-    let outputDirectory = (clone gitUrl targetDirectory) |> readOutputDirectory
+    let outputDirectory =
+        (clone gitUrl targetDirectory)
+        |> readOutputDirectory
 
     let path = Path.Combine(targetDirectory, outputDirectory)
 
-    explorer path
 
-    fork path
+    if mergeRequest then
+        rider path
+    else
+    
+        explorer path
+        fork path
 
     printfn $"Project cloned into %s{path}"
-    10. |> TimeSpan.FromSeconds |> Thread.Sleep
+
+    10.
+    |> TimeSpan.FromSeconds
+    |> Thread.Sleep
 
     0

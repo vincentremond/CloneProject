aa4fb88e7bbfb892c3e35c4e9bd27a4c6810f80b
Vincent Rémond <vincent.remond@gmail.com>
2025-12-01 14:43:04 +0100

    fix: Fix paths with exe

--- CloneProject/Program.fs
+++ CloneProject/Program.fs
@@ -16,21 +16,20 @@ module Process =
         Process.Start(ProcessStartInfo(name, arguments, WorkingDirectory = directory)).WaitForExit()
 
 let explorer (path: string) =
-    Process.start "explorer.exe" path [ path ]
+    Process.start "explorer" path [ path ]
 
-let fork (path: string) = Process.start "fork.exe" path [ path ]
+let fork (path: string) = Process.start "fork" path [ path ]
 
 let rider (path: string) =
-    // Process.start "powershell.exe" path [ "-Command"; "rider.ps1" ]
-    Process.start "rider.exe" path [ path ]
+    Process.start "rider" path [ path ]
 
-let wt (path: string) = Process.start "wt.exe" path []
+let wt (path: string) = Process.start "wt" path []
 
 let riderFixConfig (path: string) =
-    Process.start "RiderFixConfig.exe" path [ path ]
+    Process.start "RiderFixConfig" path [ path ]
 
 let clone gitUrl targetDirectory =
-    use proc = new ProcessHost("git.exe", targetDirectory)
+    use proc = new ProcessHost("git", targetDirectory)
 
     proc.Start($"clone {gitUrl}")
 
